2b7e83c1c4bfea0b90013faa9bd46e4d9460e839
Every-Morning <2529662127@qq.com>
2020-12-30 17:27:03 +0800

    第二次

--- src/main/java/com/hrj/controller/RegisterController.java
+++ src/main/java/com/hrj/controller/RegisterController.java
@@ -5,7 +5,9 @@ import com.hrj.constant.ResponseJson;
 import com.hrj.pojo.User;
 import com.hrj.service.impl.UserServiceImpl;
 import com.hrj.utils.JsonUtils;
+import com.hrj.vo.UserVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,21 +52,20 @@ public class RegisterController {
      * @return
      */
     @PostMapping("/reg_login")
-    public String regLogin(String userAccountNum,String code){
+    public JsonUtils regLogin(String userAccountNum,String code){
         JsonUtils jsonUtils = new JsonUtils(); // 将返回信息封装
         String v=userService.getCode(userAccountNum);
-
+        User user=userService.selectByUserAccountNum(userAccountNum);
         // 判断用户输入的验证码是否与redis缓存中的验证码是否一致
         if(code.equals(v)){
             // 验证数据库中是否已经存在该用户,存在直接登录
-            if(userService.selectByUserAccountNum(userAccountNum)!=null){
+            if(user!=null){
 
             jsonUtils.setStatus(ResponseJson.SUCCESS_STATUS);
             jsonUtils.setMsg("登入成功");
 
             }// 用户不存在自动注册
             else {
-                User user = new User();
                 user.setUserId(userAccountNum);
                 // 以当前时间为默认用户名
                 String userName=String.valueOf(System.currentTimeMillis());
@@ -77,23 +78,30 @@ public class RegisterController {
                 log.info("数据库插入返回值：" + i);
                 jsonUtils.setStatus(ResponseJson.SUCCESS_STATUS);
                 jsonUtils.setMsg("注册成功");
+
             }
         }else{
             jsonUtils.setStatus(ResponseJson.ERROR_STATUS);
             jsonUtils.setMsg("验证码无效");
         }
+        UserVo userVo= new UserVo();
+        BeanUtils.copyProperties(user,userVo);
+        jsonUtils.setData(userVo);
         log.info(v);
         log.info(JSON.toJSONString(jsonUtils));
-        return  JSON.toJSONString(jsonUtils);
+        return jsonUtils;
     }
 
     @PostMapping("/login")
-    public String login(String userAccountNum,String userPassword){
+    public JsonUtils login(String userAccountNum,String userPassword){
         JsonUtils jsonUtils= new JsonUtils();
+        UserVo userVo= new UserVo();
         // 查询该用户的信息
         User user=userService.selectByUserAccountNum(userAccountNum);
         if(user!=null){
             if(userPassword.equals(user.getUserPassword())){
+                BeanUtils.copyProperties(user,userVo);
+                jsonUtils.setData(userVo);
                jsonUtils.setStatus(ResponseJson.SUCCESS_STATUS);
                jsonUtils.setMsg("登入成功");
             } else if(user.getUserPassword()==null){
@@ -109,9 +117,8 @@ public class RegisterController {
             jsonUtils.setStatus(ResponseJson.ERROR_STATUS);
             jsonUtils.setMsg("该用户不存在");
         }
-
-        return JSON.toJSONString(jsonUtils);
-
+        log.info(JSON.toJSONString(jsonUtils));
+        return jsonUtils;
     }
 
 
--- src/main/java/com/hrj/utils/JsonUtils.java
+++ src/main/java/com/hrj/utils/JsonUtils.java
@@ -7,13 +7,6 @@ public class JsonUtils {
 
     public String msg;    // 返回的信息
 
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
 
     //    public User user;
     public Object data; // 返回的数据
@@ -33,6 +26,14 @@ public class JsonUtils {
 
     public void setMsg(String msg) {
         this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
     }
 
 //    public User getUser() {
